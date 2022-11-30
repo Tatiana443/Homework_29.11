44154777dd6d6c3cef9f9e76b85d024649500dc0
Tatiana443 <tvb399@yandex.ru>
2022-11-30 16:54:14 +0600

    Project Task41

---
+++ Task_41/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введите количестов чисел, которые будет введены с клавиатуры:\t");
+int elemetCount = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[elemetCount];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"\nВведите элемент массива под индексом {i} :\t ");
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+void PrintArray(int[] myArray)
+{
+    Console.Write("Вывод массива: ");
+
+    for (int i = 0; i < myArray.Length; i++)
+        Console.WriteLine($"\n{myArray[i]}");
+
+}
+int GetPositiveElem(int[] myArray)
+{
+    int sum = 0;
+    for (int i = 0; i < myArray.Length; i++)
+        if (myArray[i] > 0) sum += 1;
+    return sum;
+}
+PrintArray(myArray);
+int PositiveElem = GetPositiveElem(myArray);
+Console.WriteLine($"Положительных чисел в массиве = {PositiveElem}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

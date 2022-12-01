6762da23e5502097cd6954a344ac68c454f48574
Tatiana443 <tvb399@yandex.ru>
2022-12-01 14:38:20 +0600

    Project Task43

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -7,7 +7,16 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число k2:");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (-b2 + b1) / (-k1 + k2);
-double y = k2 * x + b2;
-
-Console.WriteLine($"Две прямые пересекутся в точке с координатами ({x},{y})");
+double FindX(double k1, double b1, double k2, double b2)
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    return x;
+}
+double x = Math.Round(FindX(k1, b1, k2, b2), 1, MidpointRounding.ToZero);
+double FindY(double k2, double b2, double x)
+{
+    double y = k2 * x + b2;
+    return y;
+}
+double y = FindY(k2, b2, x);
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты ({x},{y})");
